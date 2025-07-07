01312fab0906318c54244bf79de65759988a94bd
hakimi122 <tuanlvhe176257@fpt.edu.vn>
2025-07-07 17:18:53 +0700

    backend code update

--- app/src/main/java/com/example/finalprm392/CheckList.java
+++ app/src/main/java/com/example/finalprm392/CheckList.java
@@ -1,14 +1,97 @@
 package com.example.finalprm392;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.example.finalprm392.Adapter.CheckListAdapter;
+import com.example.finalprm392.Constants.MyConstants;
+import com.example.finalprm392.Database.RoomDB;
+import com.example.finalprm392.Models.Items;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CheckList extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    CheckListAdapter checkListAdapter;
+    RoomDB database;
+    List<Items> itemsList = new ArrayList<>();
+    String header, show;
+
+    EditText txtAdd;
+    Button  btnAdd;
+    LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_list);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Intent intent = getIntent();
+        header = intent.getStringExtra(MyConstants.HEADER_SMALL);
+        show = intent.getStringExtra(MyConstants.SHOW_SMALL);
+
+        getSupportActionBar().setTitle(header);
+
+        txtAdd = findViewById(R.id.txtAdd);
+        btnAdd = findViewById(R.id.btnAdd);
+        recyclerView = findViewById(R.id.recyclerView);
+        linearLayout = findViewById(R.id.linearLayout);
+
+        database = RoomDB.getInstance(this);
+
+        if(MyConstants.FALSE_STRING.equals(show)){
+            linearLayout.setVisibility(View.GONE);
+            itemsList = database.mainDAO().getAllSelected(true);
+        }else {
+            itemsList = database.mainDAO().getAll(header);
+        }
+
+        updateRecycler(itemsList);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String itemName = txtAdd.getText().toString();
+                if(itemName != null && !itemName.isEmpty()){
+                    addNewItem(itemName);
+                    Toast.makeText(CheckList.this, "Item Added.", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(CheckList.this, "Empty cannot be added.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void addNewItem(String itemName){
+        Items items = new Items();
+        items.setChecked(false);
+        items.setCategory(header);
+        items.setItemname(itemName);
+        items.setAddedby(MyConstants.USER_SMALL);
+        database.mainDAO().saveItem(items);
+        itemsList = database.mainDAO().getAll(header);
+        updateRecycler(itemsList);
+        recyclerView.scrollToPosition(checkListAdapter.getItemCount()-1);
+        txtAdd.setText("");
+    }
+    private void updateRecycler(List<Items> itemsList){
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(1, LinearLayoutManager.VERTICAL));
+        checkListAdapter=new CheckListAdapter(CheckList.this,itemsList,database,show);
+        recyclerView.setAdapter(checkListAdapter);
+
     }
 }
