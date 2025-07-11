5f4aff2f5e3eabce4a7462869f6a51feacb3474a
Khiem <khiemdghe161336@fpt.edu.vn>
2025-07-11 18:19:53 +0700

    Add SplashScreen

--- app/src/main/java/com/example/finalprm392/AboutUs.java
+++ app/src/main/java/com/example/finalprm392/AboutUs.java
@@ -1,6 +1,11 @@
 package com.example.finalprm392;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AboutUs extends AppCompatActivity {
+
+    ImageView imgYoutube;
+    TextView txtEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,23 @@ public class AboutUs extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("About us");
+
+        imgYoutube = findViewById(R.id.imgYoutube);
+        txtEmail = findViewById(R.id.txtEmail);
+
+        imgYoutube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.youtube.com/watch?v=xvFZjo5PgG0&list=RDxvFZjo5PgG0&start_radio=1"));
+                startActivity(intent);
+            }
         });
     }
 }
